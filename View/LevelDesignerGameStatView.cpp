27fee78ebd73060484e32c88abc8f74781a871d4
vmichele <michelet.valentin@gmail.com>
2014-06-08 18:12:20 +0200

    Add triforce level to test.

---
+++ View/LevelDesignerGameStatView.cpp
@@ -0,0 +1,5 @@
+#include "LevelDesignerGameStatView.h"
+
+LevelDesignerGameStatView::LevelDesignerGameStatView()
+{
+}
---
+++ View/LevelDesignerGameStatView.h
@@ -0,0 +1,10 @@
+#ifndef LEVELDESIGNERGAMESTATVIEW_H
+#define LEVELDESIGNERGAMESTATVIEW_H
+
+class LevelDesignerGameStatView
+{
+public:
+  LevelDesignerGameStatView();
+};
+
+#endif // LEVELDESIGNERGAMESTATVIEW_H

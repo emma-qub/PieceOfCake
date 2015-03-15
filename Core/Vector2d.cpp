ad10b1c01e1a3efcc7accf9830d5983a2648f8b7
vmichele <michelet.valentin@gmail.com>
2015-03-15 18:34:33 +0100

    Add debug information to know where the code rises an exception. Cutting through one polygon's edge is definitely not a good idea. I'll have to carrefully prevent it from happening.

--- Core/Vector2d.cpp
+++ Core/Vector2d.cpp
@@ -104,7 +104,7 @@ float Vector2d::squaredNorm(void) const {
 Vector2d Vector2d::normalized(void) const {
   float vectorNorm = norm();
   if (vectorNorm == 0.f)
-    throw std::runtime_error("Vector is nul, cannot normalize it.");
+    throw std::runtime_error("Vector is nul, cannot normalize it. (Vector2d Vector2d::normalized(void) const)");
 
   float n = 1.0 / vectorNorm;
   return Vector2d(_coords[0]*n, _coords[1]*n);
@@ -113,7 +113,7 @@ Vector2d Vector2d::normalized(void) const {
 Vector2d& Vector2d::normalize(void) {
   float vectorNorm = norm();
   if (vectorNorm == 0.f)
-    throw std::runtime_error("Vector is nul, cannot normalize it.");
+    throw std::runtime_error("Vector is nul, cannot normalize it. (Vector2d& Vector2d::normalize(void))");
 
   float n = 1.0 / vectorNorm;
   _coords[0] *= n;
